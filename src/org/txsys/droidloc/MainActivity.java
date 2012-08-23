fbd737f71322eed3028f92123dcaf655d0342dbe
jiangbei <jiangbei.wei@gmail.com>
2012-08-23 15:16:56 +0800

    implements LocationListener.

--- src/org/txsys/droidloc/MainActivity.java
+++ src/org/txsys/droidloc/MainActivity.java
@@ -1,18 +1,30 @@
 package org.txsys.droidloc;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements LocationListener {
+    
+    private LocationManager lmgr;
+    private TextView outputView;
+    private String bestProvider;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        
+        lmgr = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        outputView = (TextView) this.findViewById(R.id.output);
+        
+        Criteria criteria = new Criteria();
     }
 
     @Override
@@ -24,12 +36,28 @@ public class MainActivity extends Activity {
     
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
+//        switch (item.getItemId()) {
+//            case android.R.id.home:
+//                NavUtils.navigateUpFromSameTask(this);
+//                return true;
+//        }
         return super.onOptionsItemSelected(item);
+    }
+    
+    public void onProviderEnabled(String provider) {
+        
+    }
+    
+    public void onProviderDisabled(String provider) {
+        
+    }
+    
+    public void onLocationChanged(Location location) {
+        
+    }
+    
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        
     }
 
 }
